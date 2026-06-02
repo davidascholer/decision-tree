26f8f600f6afd009e2a9078273ab5432ee1d787a
David Scholer <davidascholer@gmail.com>
2026-06-02 20:49:30 +0000

    Generated by Spark: The editor items keep changing color. I think there is some sort of lingering effect from a deprecated color setting. Make sure to remove all of the old color settings and just keep the fixed colors specified in the last commit.

--- src/hooks/use-node-colors.ts
+++ src/hooks/use-node-colors.ts
@@ -1,7 +1,5 @@
-import { useKV } from '@github/spark/hooks'
 import { NodeColors, DEFAULT_COLORS } from '@/lib/node-colors'
 
 export function useNodeColors(): NodeColors {
-  const [colors] = useKV<NodeColors>('node-colors', DEFAULT_COLORS)
-  return colors || DEFAULT_COLORS
+  return DEFAULT_COLORS
 }
